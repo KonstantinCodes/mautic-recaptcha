d19997e27bcf75aa0285727cd179baa0bdea674f
Konstantin Scheumann <konstantin@konstantin.codes>
2018-01-03 13:45:46 +0200

    Add License

--- MauticRecaptchaBundle/Form/Type/RecaptchaType.php
+++ MauticRecaptchaBundle/Form/Type/RecaptchaType.php
@@ -3,6 +3,7 @@
 /*
  * @copyright   2018 Konstantin Scheumann. All rights reserved
  * @author      Konstantin Scheumann
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 namespace MauticPlugin\MauticRecaptchaBundle\Form\Type;
